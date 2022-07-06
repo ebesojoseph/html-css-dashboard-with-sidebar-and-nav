3dd7dcab5ede4251e0e6f663aa4a058ea85abdf9
ebesojoseph <codewithebs@gmail.com>
2022-07-06 06:28:46 +0100

    handling closing

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 var closeBtn = document.querySelector(".close-btn");
 var sidebar = document.querySelector(".sidebar");
+var logo = document.querySelector(".logo");
 
 closeBtn.addEventListener("click", (e) => {
   console.log("clicked");
   sidebar.classList.add("close");
 });
+
+logo.addEventListener("click", (e) => {
+  console.log(e);
+  sidebar.classList.remove("close");
+});
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.querySelector(".nav-bar").style.position = "fixed";
+  } else {
+    document.querySelector(".nav-bar").style.display = "relative";
+  }
+}
